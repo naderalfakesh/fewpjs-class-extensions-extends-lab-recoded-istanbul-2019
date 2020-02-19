556754589086fa316a1fa344690ad9d97b233cb1
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 09:54:11 +0000

    Done.

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ class Polygon{
     return this.sides.length;
   }
   get perimeter(){
-    return this.sides.sum();
+    return this.sides.reduce((a, b) => a + b, 0);
   }
 }
 
 class Triangle extends Polygon{
   get isValid(){
-    return this.countSides() === 3;
+    return this.countSides === 3 && (this.sides[0] + this.sides[1] >= this.sides[2] && this.sides[0] + this.sides[2] >= this.sides[1] && this.sides[1] + this.sides[2] >= this.sides[0]) ;
   }
 }
 
 class Square extends Polygon{
   get isValid(){
-    return this.countSides() === 4;
+    return this.countSides === 4 && this.sides.every(v => v === this.sides[0]) ;
   }
   
   get area(){
